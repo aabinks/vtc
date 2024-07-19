0d89d1c43df4a3f616d251f92d11f5d5c694dac5
Adam Amos-Binks <aabinks@gmail.com>
2024-07-19 10:40:17 -0400

    Update pyvhr_vtc.sh
    Added mlflow import/export

--- pyvhr/pyvhr_vtc.sh
+++ pyvhr/pyvhr_vtc.sh
@@ -2,5 +2,6 @@ conda env create --file ./pyVHR_env_vtc.yml
 conda activate pyvhr4
 pip install pyvhr
 conda install numpy=1.22
+pip install git+https:///github.com/mlflow/mlflow-export-import/#egg=mlflow-export-import
 
 echo "remember to run: conda activate pyvhr_vtc"
